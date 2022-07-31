7ca314247cd0777f80138dc1c2063b8eadcbbc4e
Jinseok Son <sjs2666@gmail.com>
2022-08-01 00:37:41 +0900

    Refactoring

--- test.py
+++ test.py
@@ -1,45 +1,35 @@
 import pandas as pd
-import torch
-from sklearn.preprocessing import LabelEncoder
 from transformers import BertTokenizer, TrainingArguments, Trainer
 
 from model import BertSrcClassifier
-from utils import SrcDataset, compute_metrics, CLASSES
+from utils import preprocess_data, compute_metrics, CLASSES
 
 
-test = pd.read_csv("data/test.csv", encoding='utf-8')
+if __name__ == "__main__":
+    test = pd.read_csv("data/test.csv", encoding="utf-8")
 
-tokenizer = BertTokenizer.from_pretrained(
-    "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
-)
-model = BertSrcClassifier.from_pretrained(
-    "./checkpoints/best_model",
-    num_labels=len(CLASSES),
-    mask_token_id=tokenizer.mask_token_id,
-)
+    tokenizer = BertTokenizer.from_pretrained(
+        "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+    )
+    model = BertSrcClassifier.from_pretrained(
+        "./checkpoints/best_model",
+        num_labels=len(CLASSES),
+        mask_token_id=tokenizer.mask_token_id,
+    )
 
-test_x1 = test.apply(lambda x: x["14"].replace(x["3"], "[MASK]"), axis=1).tolist()
-test_x2 = test.apply(lambda x: x["14"].replace(x["8"], "[MASK]"), axis=1).tolist()
+    test_dataset = preprocess_data(test, tokenizer, x_col="14", y_col="16", e1_col="3", e2_col="8")
 
-label_encoder = LabelEncoder()
-test_y = torch.tensor(label_encoder.fit_transform(test["16"]), dtype=torch.long)
+    training_args = TrainingArguments(
+        output_dir="./checkpoints",
+        logging_dir="./logs",
+        per_device_eval_batch_size=64,
+    )
 
-tokenized_test = tokenizer(
-    test_x1, test_x2, return_tensors="pt", padding=True, truncation=True
-)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        eval_dataset=test_dataset,
+        compute_metrics=compute_metrics,
+    )
 
-test_dataset = SrcDataset(tokenized_test, test_y)
-
-training_args = TrainingArguments(
-    output_dir="./checkpoints",
-    per_device_eval_batch_size=64,
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    eval_dataset=test_dataset,
-    compute_metrics=compute_metrics,
-)
-
-trainer.evaluate()
+    trainer.evaluate()
